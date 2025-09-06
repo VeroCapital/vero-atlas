a46e0bc344b5c06840835ca14cf73d57bab0f386
VeroCapital <lassemango@gmail.com>
2025-09-06 08:02:04 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // This project uses the app directory. See https://nextjs.org/docs/app for more details.
+    appDir: true
+  }
+};
+
+module.exports = nextConfig;
