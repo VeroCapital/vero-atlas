746004aaa09fa4603af695ecf14a865660bc3804
VeroCapital <lassemango@gmail.com>
2025-09-07 06:37:47 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // This project uses the app directory. See https://nextjs.org/docs/app for more details.
-    appDir: true
-  }
+  images: { unoptimized: true },
+  output: 'export'
 };
 
 module.exports = nextConfig;
